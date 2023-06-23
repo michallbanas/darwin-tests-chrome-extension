f4a56cc797ee3318b1255644c01b451e22ee064b
Michal Banas <banas.michal@icloud.com>
2023-06-23 14:27:03 +0200

    chore: uninstall cypress (#25)
    * chore: update version to 0.6.2
    
    * chore: uninstall cypress
    
    * fix: remove unnecessary playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,8 @@ const { defineConfig, devices } = require("@playwright/test")
 module.exports = defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  name: "Integration tests for Darwin A/B Tests Chrome Extension",
+  reporter: "null",
 
   projects: [
     {
