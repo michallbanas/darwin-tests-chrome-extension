417d3b1d39b717090f727f4e2953e0a19eecf395
Michal Banas <banas.michal@icloud.com>
2023-08-09 16:04:33 +0200

    fix: horizontal scrollbar is not shown when usb mouse is plugged in (#32)

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setTitle({ title: "Click to open Darwin Test Extension" })
   chrome.contextMenus.create({
     id: "extension-version",
-    title: "Darwin Test Extension v0.7.0",
+    title: "Darwin Test Extension v0.7.1",
     contexts: ["all"],
   })
 
