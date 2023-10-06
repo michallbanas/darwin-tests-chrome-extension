22e0e7ac9afcae6d48bf031c6f13a61547a75367
Michal Banas <banas.michal@icloud.com>
2023-10-06 19:50:24 +0200

    fix: search input light mode (#38)
    * fix: search input in light mode
    
    * chore: update version

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setTitle({ title: "Click to open Darwin Test Extension" })
   chrome.contextMenus.create({
     id: "extension-version",
-    title: "Darwin Test Extension v0.8.0",
+    title: "Darwin Test Extension v0.8.1",
     contexts: ["all"],
   })
 
