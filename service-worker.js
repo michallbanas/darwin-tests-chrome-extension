b4fe96915f1227bcd78fbc0dee1f73a5c9433533
michalbanas <michal.banas@kiwi.com>
2024-01-23 14:50:02 +0100

    chore: update deps

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setTitle({ title: "Click to open Darwin Test Extension" })
   chrome.contextMenus.create({
     id: "extension-version",
-    title: "Darwin Test Extension v0.8.5",
+    title: "Darwin Test Extension v0.8.6",
     contexts: ["all"],
   })
 
